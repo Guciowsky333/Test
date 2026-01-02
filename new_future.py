523cc65e41843c184832e1a439ad7456f514d706
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-01-02 18:31:18 +0100

    Add function potegi

---
+++ new_future.py
@@ -0,0 +1,9 @@
+x = int(input('Podaj liczbe:'))
+y = int(input('Podaj potege do ktorej chcesz podniesc ta liczbe :'))
+
+def potegi(x, y) -> int:
+    if x or y ==0:
+        raise Exception('liczby nie moge byc 0')
+    return x ** y
+print(potegi(x, y))
+
